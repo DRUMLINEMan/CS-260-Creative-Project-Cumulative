d1692bb5e08257ff6eeeb23361db5f71890a71fe
EC2 Default User <ec2-user@ip-172-31-21-136.us-east-2.compute.internal>
2019-10-09 01:39:45 +0000

    API added

--- js/select.js
+++ js/select.js
@@ -1,3 +1,5 @@
+/* global fetch */
+
 var meals = [{
     "name":"Cheeseburger",
     "img":"burgerTest"
@@ -36,37 +38,48 @@ function select(id) {
 }
 
 function fill(){
-    var eachCol = meals.length / 3;
-    var end = 0;
-    for(let i = 0; i < 3; i++){
-        var column = document.createElement("div");
-        column.setAttribute("class", "flex-md-fill");
-        var list = document.createElement("ul");
-        end += eachCol;
-        if(list.length < end){
-            end = list.length;
-        }
-        for(let j = i * eachCol; j < end; j++){
-            var item = document.createElement("li");
-            item.setAttribute("class", "select-meal-item");
-            item.setAttribute("id", j);
-            item.setAttribute("onclick", "select(" + j + ")");
-            var innerDiv = document.createElement("div");
-            innerDiv.setAttribute("class", "d-flex");
-            var innerDivHtml = "<img src=\"";
-            if(meals[j].img != ""){
-                innerDivHtml += "images/" + meals[j].img + ".jpg";
+    const url = "https://api.spoonacular.com/recipes/search?query=burger&number=" + meals.length + "&apiKey=6defd8977ec24ae9829533ab61cdc90a";
+      fetch(url)
+        .then(function(response) {
+          return response.json();
+        }).then(function(json) {
+            console.log(json);
+            for(let i = 0; i < json.results.length; i++){
+                meals[i].name = json.results[i].title;
+                meals[i].img = json.baseUri + json.results[i].image;
             }
-            else{
-                innerDivHtml += "http://alumni.byu.edu/sites/all/themes/byu2016/sites/default/favicon.ico";
+            var eachCol = meals.length / 3;
+            var end = 0;
+            for(let i = 0; i < 3; i++){
+                var column = document.createElement("div");
+                column.setAttribute("class", "flex-md-fill");
+                var list = document.createElement("ul");
+                end += eachCol;
+                if(list.length < end){
+                    end = list.length;
+                }
+                for(let j = i * eachCol; j < end; j++){
+                    var item = document.createElement("li");
+                    item.setAttribute("class", "select-meal-item");
+                    item.setAttribute("id", j);
+                    item.setAttribute("onclick", "select(" + j + ")");
+                    var innerDiv = document.createElement("div");
+                    innerDiv.setAttribute("class", "d-flex");
+                    var innerDivHtml = "<img src=\"";
+                    if(meals[j].img != ""){
+                        innerDivHtml += meals[j].img;
+                    }
+                    else{
+                        innerDivHtml += "http://alumni.byu.edu/sites/all/themes/byu2016/sites/default/favicon.ico";
+                    }
+                    innerDivHtml += "\" height=\"100px\" width=\"100px\" class=\"meal-img\" />&nbsp;&nbsp;&nbsp;&nbsp;<div class=\"align-self-center\"><h4>" + meals[j].name + "</h4></div>";
+                    innerDiv.innerHTML = innerDivHtml;
+                    item.appendChild(innerDiv);
+                    column.appendChild(item);
+                }
+                document.getElementById("select-meal").appendChild(column);
             }
-            innerDivHtml += "\" height=\"100px\" class=\"meal-img\" />&nbsp;&nbsp;&nbsp;&nbsp;<div class=\"align-self-center\"><h4>" + meals[j].name + "</h4></div>";
-            innerDiv.innerHTML = innerDivHtml;
-            item.appendChild(innerDiv);
-            column.appendChild(item);
-        }
-        document.getElementById("select-meal").appendChild(column);
-    }
+        });
 }
 
 fill();
